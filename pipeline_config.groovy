e8a906de3746625809bc605307ec48063cecc57e
Alec Keller <keller_alec@bah.com>
2018-12-17 10:45:11 -0500

    newman config

--- pipeline_config.groovy
+++ pipeline_config.groovy
@@ -1,1 +1,9 @@
 organization = "SDP-Apps-Organization"
+libraries{
+  newman{
+    dev_Run_Newman_Tests: true
+    test_Run_Newman_Tests: true
+    staging_Run_Newman_Tests: true
+    prod_Run_Newman_Tests: true
+  }
+}
