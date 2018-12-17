3867b3b3d6ebec71682c27b87e49e850d36221ea
Alec Keller <keller_alec@bah.com>
2018-12-17 10:49:29 -0500

    changed semi colons

--- pipeline_config.groovy
+++ pipeline_config.groovy
@@ -1,9 +1,9 @@
 organization = "SDP-Apps-Organization"
 libraries{
   newman{
-    dev_Run_Newman_Tests: true
-    test_Run_Newman_Tests: true
-    staging_Run_Newman_Tests: true
-    prod_Run_Newman_Tests: true
+    dev_Run_Newman_Tests = true
+    test_Run_Newman_Tests = true
+    staging_Run_Newman_Tests = true
+    prod_Run_Newman_Tests = true
   }
 }
