78136a65ddbcd40fabe30a471aee8af56b1f9ce4
aleckeller <alec.keller.17@gmail.com>
2018-12-18 13:33:49 -0500

    Update pipeline_config.groovy

--- pipeline_config.groovy
+++ pipeline_config.groovy
@@ -6,4 +6,10 @@ libraries{
     staging_Run_Newman_Tests = true
     prod_Run_Newman_Tests = true
   }
+  syntax_unit_testing{
+    dev_Skip_Tests = true
+    test_Skip_Tests = true
+    staging_Skip_Tests = true
+    prod_Skip_Tests = true
+  }
 }
