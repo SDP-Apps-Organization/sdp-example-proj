bafc6c70fc993c9786ab59bbdb46306f02066504
Alec Keller <keller_alec@bah.com>
2019-01-09 15:03:20 -0500

    turned off tests

--- pipeline_config.groovy
+++ pipeline_config.groovy
@@ -1,10 +1,10 @@
 organization = "SDP-Apps-Organization"
 libraries{
   newman{
-    dev_Run_Newman_Tests = true
-    test_Run_Newman_Tests = true
-    staging_Run_Newman_Tests = true
-    prod_Run_Newman_Tests = true
+    dev_Run_Newman_Tests = false
+    test_Run_Newman_Tests = false
+    staging_Run_Newman_Tests = false
+    prod_Run_Newman_Tests = false
   }
   syntax_unit_testing{
     dev_Skip_Tests = true
