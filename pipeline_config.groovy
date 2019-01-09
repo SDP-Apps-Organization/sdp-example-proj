884ff7a73d7d7ee02cb0047e10439f8f8e739da3
aleckeller <alec.keller.17@gmail.com>
2019-01-09 14:12:51 -0500

    Update pipeline_config.groovy

--- pipeline_config.groovy
+++ pipeline_config.groovy
@@ -1,7 +1,7 @@
 organization = "SDP-Apps-Organization"
 libraries{
   newman{
-    dev_Run_Newman_Tests = true
+    dev_Run_Newman_Tests = false
     test_Run_Newman_Tests = true
     staging_Run_Newman_Tests = true
     prod_Run_Newman_Tests = true
